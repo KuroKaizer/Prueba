f2c5f2a61eab3874a6e2cbc876dae0e6191c2adf
Manuel <manueludouni@gmail.com>
2023-05-22 14:02:23 -0400

    Creación de Modelos

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ SECRET_KEY = "f23943811630ad5cb3b8b2d1787acb0dbc7b55911bacf9b21552c9615957f8c6"
 ALGORITHM = "HS256"
 ACCES_TOKEN_EXPIRE_MINUTES = 30
 
-app = FastAPI()
 
 fake_db = {
     "Manuel": {
@@ -20,3 +19,25 @@ fake_db = {
         "disabled": False
     }
 }
+
+class Token(BaseModel):
+    acces_token: str
+    token_type: str
+    
+class TokenData(BaseModel):
+    username: str
+    
+class User(BaseModel):
+    username: str
+    email: str or None = None
+    full_name: str or None = None
+    disabled: bool or None = None
+    
+class UserInDB(User):
+    hashed_password: str 
+    
+
+pwd_context = CryptContext(schemes=["bcrypt"], depreacted="auto")
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()               
