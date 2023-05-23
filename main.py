06d96391e1e047c98fc4545183253c2ba6212fa0
Manuel <manueludouni@gmail.com>
2023-05-23 05:57:39 -0400

    Función para tener acceso al token

--- main.py
+++ main.py
@@ -64,4 +64,16 @@ def authenticate_user(db, username: str, password: str):
     if not verify_password(password, user.hashed_password):
         return False
     
-    return user                    
+    return user      
+
+
+def create_access_token(data: dict, expires_delta: timedelta or None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+        
+    to_encode.update({"exp": expire})                     
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
